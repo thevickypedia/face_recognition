cc7f9241293b55b7aeff436684566efb047a025a
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-25 11:51:26 -0500

    load validation images and render face recognition matrix

--- recognizer.py
+++ recognizer.py
@@ -1,9 +1,10 @@
 import face_recognition
 import os
 
-training_dataset = "train"
+training_dataset, validation_dataset = "train", "valid"
+model = 'hog'
 
-train_faces = []
+train_faces, train_names = [], []
 
 for character_dir in os.listdir(training_dataset):
     for file_name in os.listdir(f'{training_dataset}/{character_dir}'):
@@ -11,4 +12,8 @@ for character_dir in os.listdir(training_dataset):
         encoded = face_recognition.face_encodings(img)[0]
         train_faces.append(encoded)
 
-print(train_faces)
+for file_name in os.listdir(validation_dataset):
+    img = face_recognition.load_image_file(f'{validation_dataset}/{file_name}')
+    identifier = face_recognition.face_locations(img, model=model)
+    encoded_ = face_recognition.face_encodings(img, identifier)
+    print(encoded_)
