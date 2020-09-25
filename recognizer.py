7c6163aa6cca3a7d759e47d1ff2b4449aa031cfa
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-25 10:36:55 -0500

    load images and render face recognition matrix

--- recognizer.py
+++ recognizer.py
@@ -1,7 +1,14 @@
+import face_recognition
 import os
 
 training_dataset = "train"
 
+train_faces = []
+
 for character_dir in os.listdir(training_dataset):
     for file_name in os.listdir(f'{training_dataset}/{character_dir}'):
-        print(file_name)
+        img = face_recognition.load_image_file(f'{training_dataset}/{character_dir}/{file_name}')
+        encoded = face_recognition.face_encodings(img)[0]
+        train_faces.append(encoded)
+
+print(train_faces)
